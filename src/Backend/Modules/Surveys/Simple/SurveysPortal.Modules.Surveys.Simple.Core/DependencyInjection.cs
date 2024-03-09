8a2ae35a94ea8afc9e4274a865dc308d1a7a4460
MALIGLOK <karol.maliglowka@zf.com>
2024-03-09 22:03:09 +0100

    changes after review

--- src/Backend/Modules/Surveys/Simple/SurveysPortal.Modules.Surveys.Simple.Core/DependencyInjection.cs
+++ src/Backend/Modules/Surveys/Simple/SurveysPortal.Modules.Surveys.Simple.Core/DependencyInjection.cs
@@ -11,6 +11,7 @@ public static class DependencyInjection
             .FromCallingAssembly()
             .AddClasses()
             .InjectableAttributes());
+        
         return services;
     }
 }
--- src/Backend/Modules/Users/SurveysPortal.Modules.Users.Api/DependencyInjection.cs
+++ src/Backend/Modules/Users/SurveysPortal.Modules.Users.Api/DependencyInjection.cs
@@ -11,6 +11,7 @@ public static class DependencyInjection
         services
             .AddUsersCoreLayer()
             .AddUsersInfrastructureLayer();
+        
         return services;
     }
 }

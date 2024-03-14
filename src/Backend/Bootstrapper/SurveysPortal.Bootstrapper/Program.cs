1d07315fdbb67bcf524fcf44864507b9f27d0306
MALIGLOK <karol.maliglowka@zf.com>
2024-03-14 21:33:09 +0100

    added controllers

--- src/Backend/Bootstrapper/SurveysPortal.Bootstrapper/Program.cs
+++ src/Backend/Bootstrapper/SurveysPortal.Bootstrapper/Program.cs
@@ -1,10 +1,20 @@
+using Microsoft.OpenApi.Models;
 using SurveysPortal.Modules.Notifications.Api;
 using SurveysPortal.Modules.Surveys.QuestionAnswer.Api;
 using SurveysPortal.Modules.Surveys.Simple.Api;
 using SurveysPortal.Modules.Users.Api;
 
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Survey's Portal API",
+            Version = "v1"
+        });
+    });
 builder.Services
     .AddUsersModule()
     .AddSurveysQuestionAnswerModule()
@@ -12,7 +22,15 @@ builder.Services
     .AddNotificationsModule();
 
 var app = builder.Build();
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-//app.MapGet("/", () => "Hello World!");
+// app.UseHttpsRedirection();
+// app.UseExceptionHandler("/error");
+// app.UseRouting();
+// app.MapControllers();
 
 app.Run();

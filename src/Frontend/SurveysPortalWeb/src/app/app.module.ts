7e9ebde8b9a6e013193c7960f4128c2981692268
MALIGLOK <karol.maliglowka@zf.com>
2024-03-19 21:01:48 +0100

    added users list
    Signed-off-by: MALIGLOK <karol.maliglowka@zf.com>

--- src/Frontend/SurveysPortalWeb/src/app/app.module.ts
+++ src/Frontend/SurveysPortalWeb/src/app/app.module.ts
@@ -1,13 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { UsersListComponent } from './users/users-list/users-list.component';
-import { UserDetailsComponent } from './users/user-details/user-details.component';
-import { ButtonModule } from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {UsersListComponent} from './users/users-list/users-list.component';
+import {UserDetailsComponent} from './users/user-details/user-details.component';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
 import {FormsModule} from "@angular/forms";
 import {RippleModule} from "primeng/ripple";
+import {TableModule} from "primeng/table";
+import {MenuModule} from "primeng/menu";
+import {ConfirmPopupModule} from "primeng/confirmpopup";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {ApiService} from "./services/api.service";
+import {HttpClientModule} from "@angular/common/http";
+import {ConfirmationService} from "primeng/api";
 
 @NgModule({
   declarations: [
@@ -21,9 +28,18 @@ import {RippleModule} from "primeng/ripple";
     ButtonModule,
     InputTextModule,
     FormsModule,
-    RippleModule
+    RippleModule,
+    TableModule,
+    MenuModule,
+    ConfirmPopupModule,
+    ConfirmDialogModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ApiService,
+    ConfirmationService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}

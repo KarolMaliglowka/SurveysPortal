f903c0e00cf81849c3839de3b62db13fc5a28d74
Karol Maligowka <karol_maliglowka@o2.pl>
2024-04-21 20:16:10 +0200

    changes

--- src/Backend/Modules/Surveys/Standard/SurveysPortal.Modules.Surveys.Standard.Application/Commands/Handlers/EditQuestionHandler.cs
+++ src/Backend/Modules/Surveys/Standard/SurveysPortal.Modules.Surveys.Standard.Application/Commands/Handlers/EditQuestionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using SurveysPortal.Modules.Surveys.Standard.Core.Exceptions;
 using SurveysPortal.Modules.Surveys.Standard.Core.Repositories;
 using SurveysPortal.Shared.Abstractions.Attributes;
 using SurveysPortal.Shared.Abstractions.Commands;
@@ -21,7 +22,12 @@ public class EditQuestionHandler : ICommandHandler<EditQuestion>
 
     public async Task HandleAsync(EditQuestion command, CancellationToken cancellationToken = default)
     {
-        
-    }
+        var question = await _questionRepository.GetStandardQuestionById(command.Question.Id);
+        if (question is null)
+        {
+            throw new QuestionNotFoundException(command.Question.Id);
+        }
 
+        await _questionRepository.Update(command.Question);
+    }
 }

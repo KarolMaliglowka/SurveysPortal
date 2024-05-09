444b3c9faba00a08731a5dc30f31fe4aace09911
MALIGLOK <karol.maliglowka@zf.com>
2024-05-09 22:05:18 +0200

    added view
    Signed-off-by: MALIGLOK <karol.maliglowka@zf.com>

--- src/Frontend/SurveysPortalWeb/src/app/surveys/standard/standard-surveys-list/standard-surveys-list.component.ts
+++ src/Frontend/SurveysPortalWeb/src/app/surveys/standard/standard-surveys-list/standard-surveys-list.component.ts
@@ -1,9 +1,34 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from "primeng/button";
+import {ConfirmDialogModule} from "primeng/confirmdialog";
+import {ConfirmPopupModule} from "primeng/confirmpopup";
+import {FileUploadModule} from "primeng/fileupload";
+import {NgForOf, NgSwitch, NgSwitchCase} from "@angular/common";
+import {RippleModule} from "primeng/ripple";
+import {SharedModule} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {ToolbarModule} from "primeng/toolbar";
+import {TooltipModule} from "primeng/tooltip";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-standard-surveys-list',
   standalone: true,
-  imports: [],
+    imports: [
+        ButtonModule,
+        ConfirmDialogModule,
+        ConfirmPopupModule,
+        FileUploadModule,
+        NgForOf,
+        NgSwitchCase,
+        RippleModule,
+        SharedModule,
+        TableModule,
+        ToolbarModule,
+        TooltipModule,
+        RouterLink,
+        NgSwitch
+    ],
   templateUrl: './standard-surveys-list.component.html',
   styleUrl: './standard-surveys-list.component.scss'
 })

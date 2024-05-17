aeb75448499b9f907bb0ae0bcd01d33ceeae2ec1
MALIGLOK <karol.maliglowka@zf.com>
2024-05-17 22:29:32 +0200

    wip - notifications
    Signed-off-by: MALIGLOK <karol.maliglowka@zf.com>

--- src/Backend/Shared/SurveysPortal.Shared.Infrastructure/Events/EventDispatcher.cs
+++ src/Backend/Shared/SurveysPortal.Shared.Infrastructure/Events/EventDispatcher.cs
@@ -4,14 +4,36 @@ using SurveysPortal.Shared.Abstractions.Events;
 
 namespace SurveysPortal.Shared.Infrastructure.Events;
 
-[Injectable(ServiceLifetime.Scoped)]
+[Injectable(ServiceLifetime.Singleton)]
 internal sealed class EventDispatcher(IServiceProvider serviceProvider) : IEventDispatcher
 {
-    public async Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default) where TEvent : class, IEvent
+    public async Task PublishAsync<TEvent>(TEvent @event, CancellationToken cancellationToken = default)
+        where TEvent : class, IEvent
     {
+        if (typeof(TEvent) == typeof(IEvent))
+        {
+            await DispatchDynamicallyAsync(@event, cancellationToken);
+            return;
+        }
+
         using var scope = serviceProvider.CreateScope();
         var handlers = scope.ServiceProvider.GetServices<IEventHandler<TEvent>>();
         var tasks = handlers.Select(x => x.HandleAsync(@event, cancellationToken));
+        await Task.WhenAll(tasks);
+    }
+
+    private async Task DispatchDynamicallyAsync(IEvent @event, CancellationToken cancellationToken = default)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var handlerType = typeof(IEventHandler<>).MakeGenericType(@event.GetType());
+        var handlers = scope.ServiceProvider.GetServices(handlerType);
+        var method = handlerType.GetMethod(nameof(IEventHandler<IEvent>.HandleAsync));
+        if (method is null)
+        {
+            throw new InvalidOperationException($"Event handler for '{@event.GetType().Name}' is invalid.");
+        }
+
+        var tasks = handlers.Select(x => (Task)method.Invoke(x, new object[] { @event, cancellationToken }));
         await Task.WhenAll(tasks);
     }
 }

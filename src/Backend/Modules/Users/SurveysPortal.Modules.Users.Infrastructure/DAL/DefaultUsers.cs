6f2a99ee9e918dc2f4a690f9d0eb5ca79aa92bea
MALIGLOK <karol.maliglowka@zf.com>
2024-05-24 19:48:39 +0200

    changes
    Signed-off-by: MALIGLOK <karol.maliglowka@zf.com>

--- src/Backend/Modules/Users/SurveysPortal.Modules.Users.Infrastructure/DAL/DefaultUsers.cs
+++ src/Backend/Modules/Users/SurveysPortal.Modules.Users.Infrastructure/DAL/DefaultUsers.cs
@@ -7,23 +7,26 @@ public class DefaultUsers(UsersDbContext context)
 {
     public async Task SeedUsers()
     {
-        var users = new List<User>
+        if (!context.Users.Any())
         {
-            new("John", "Doe", "doej", "john.doe@surveysportal.com", "Doe John"),
-            new("John", "Wick", "wickj", "john.wick@surveysportal.com", "Wick John")
-        };
+            var users = new List<User>
+            {
+                new("John", "Doe", "doej", "john.doe@surveysportal.com", "Doe John"),
+                new("John", "Wick", "wickj", "john.wick@surveysportal.com", "Wick John")
+            };
 
-        foreach (var user in users)
-        {
-            var dbUser = await context.Users
-                .FirstOrDefaultAsync(x => x.NormalizedEmail == user.NormalizedEmail);
-            if (dbUser is not null) continue;
-            await context.Users.AddAsync(user);
-        }
+            foreach (var user in users)
+            {
+                var dbUser = await context.Users
+                    .FirstOrDefaultAsync(x => x.NormalizedEmail == user.NormalizedEmail);
+                if (dbUser is not null) continue;
+                await context.Users.AddAsync(user);
+            }
 
-        if (context.ChangeTracker.HasChanges())
-        {
-            await context.SaveChangesAsync();
+            if (context.ChangeTracker.HasChanges())
+            {
+                await context.SaveChangesAsync();
+            }
         }
     }
 }
